1b8fa5e50fb717112a62e7a856f14b9cfbeca17d
asafeeson <g.kochergin@hotmail.com>
2026-01-03 00:58:06 +0400

    refactor: enhance layout and styling for contact section description

--- src/lib/components/SectionContactCta.svelte
+++ src/lib/components/SectionContactCta.svelte
@@ -4,15 +4,19 @@
 </script>
 
 <Section.Root variant="none" className="bg-black py-16 md:py-20">
-	<Section.Content className="flex-col items-center">
+	<Section.Content className="flex flex-col items-center justify-center">
 		<!-- Description Text -->
-		<p class="mb-8 text-center text-lg leading-relaxed text-white md:text-xl">
-			If you’re looking for a partner who <span class="font-bold"
-				>understands business, masters technology,</span
-			>
-			and <span class="bg-yellow-accent">uses AI where it adds real value</span>, delivering
-			reliable and practical results — let’s talk.
-		</p>
+		<div class="mb-8 max-w-2/3">
+			<p class="text-center text-lg leading-relaxed text-white md:text-xl lg:text-2xl">
+				If you’re looking for a partner who <span class="font-bold"
+					>understands business, masters technology,</span
+				>
+				and
+				<span class="bg-yellow-accent px-2 py-1 whitespace-nowrap text-black"
+					>uses AI where it adds real value</span
+				>, delivering reliable and practical results — let’s talk.
+			</p>
+		</div>
 
 		<!-- CTA Button -->
 		<Button href="mailto:asafeeson@outlook.com" variant="inverse">Get In Touch</Button>
