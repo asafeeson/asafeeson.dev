f0dd1995bb0b51c6a2cadc9568fc28804d7a43f4
asafeeson <g.kochergin@hotmail.com>
2026-01-04 14:11:27 +0400

    upd style

--- src/lib/components/ModeToggler.svelte
+++ src/lib/components/ModeToggler.svelte
@@ -3,7 +3,9 @@
 	import BulbOnIcon from '$icons/bulb-on.svg';
 	import { cn } from '$utils/utils';
 	import { toggleMode } from 'mode-watcher';
-	
+	import BulbOff from './icons/BulbOff.svelte';
+	import BulbOn from './icons/BulbOn.svelte';
+
 	interface Props {
 		className?: string;
 	}
@@ -14,19 +16,15 @@
 <button
 	onclick={toggleMode}
 	aria-label="Toggle light/dark mode"
-	class={cn("flex rounded-full border px-1 py-0.5 transition-all dark:border-orange-accent active:scale-95", className)}
+	class={cn(
+		'flex rounded-full border border-black/50 px-1 py-0.5 transition-all active:scale-95 dark:border-orange-accent',
+		className
+	)}
 >
-	<img
-		src={BulbOffIcon}
-		alt="Light mode icon"
-		class="h-5 w-5 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
-	/>
-	<img
-		src={BulbOnIcon}
-		alt="Dark mode icon"
+	<BulbOff class="h-5 w-5 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90 text-black/60" />
+	<BulbOn
 		class="h-5 w-5 scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0 dark:text-orange-accent"
 	/>
-
 	<span class="sr-only">Toggle light/dark theme</span>
 </button>
 
